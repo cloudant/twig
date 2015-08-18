28cb3b68644d8705147bb08ba69c01c56fc7f26e
Robert Newson <rnewson@apache.org>
2015-08-19 00:16:30 +0100

    Allow facility override in options
    BugzID: 32072

--- src/twig.erl
+++ src/twig.erl
@@ -29,29 +29,38 @@ log(LevelAtom, String) ->
 log(LevelAtom, Format, Data) ->
     log(LevelAtom, Format, Data, []).
 
-log(LevelAtom, Format, Data, _Options) ->
+log(LevelAtom, Format, Data, Options) ->
     %% TODO do something useful with options
     Level = twig_util:level(LevelAtom),
+    Facility = get_facility(Options),
     case application:get_env(twig, level) of
         {ok, Threshold} when Level =< Threshold ->
-            send_message(Level, Format, Data);
+            send_message(Level, Facility, Format, Data);
         undefined when Level =< ?LEVEL_INFO ->
-            send_message(Level, Format, Data);
+            send_message(Level, Facility, Format, Data);
         _ ->
             ok
     end.
 
 %% internal
 
-send_message(Level, Format, Data) ->
-    gen_event:sync_notify(error_logger, format(Level, Format, Data)).
+send_message(Level, Facility, Format, Data) ->
+    gen_event:sync_notify(error_logger, format(Level, Facility, Format, Data)).
 
-format(Level, Format, Data) ->
+format(Level, Facility, Format, Data) ->
     %% TODO truncate large messages
     #twig{
         level = Level,
+        facility = Facility,
         msg = iolist_to_binary(twig_util:format(Format, Data)),
         msgid = erlang:get(nonce),
         pid = self()
     }.
 
+get_facility(Options) ->
+    case proplists:get_value(facility, Options) of
+        undefined ->
+            undefined;
+        Facility ->
+            twig_util:facility(Facility)
+    end.
